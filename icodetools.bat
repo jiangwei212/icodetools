c72301d63137cd457050cb9d0376595768ed77d4
CORP\jiangwei1-g <1095639004@qq.com>
2016-12-02 15:54:53 +0800

    增加工具多种版本

--- icodetools.bat
+++
@@ -1,5 +0,0 @@
-cd %~dp0
-set aapt_path=D:\Android_tools\AndroidSdk\build-tools\23.0.1\aapt.exe
-java -jar libs\icode_tools.jar %~dp0 src.apk %aapt_path% jw cn.wjdiankong.jw.utils.JWUtils printStackTrace
-adb install -r signed.apk
-pause..
